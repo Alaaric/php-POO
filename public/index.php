eff537474efa98e93fc3fc5eb69787a24f1abe98
Alaric Henrot <alaric.henrot@ospi.fr>
2024-07-17 17:26:19 +0200

    add speak and threatenedLevel

--- public/index.php
+++ public/index.php
@@ -11,17 +11,29 @@ $lion->pawNumber = 4;
 $lion->carnivorous = true;
 $lion->size = 70;
 $lion->threatenedLevel = 'VU';
+echo $lion->speak('fr');
+echo $lion->speak('en');
+echo $lion->speak();
+echo $lion->isDangerous();
 
 $parrot = new Animal();
 $parrot->name = 'parrot';
 $parrot->pawNumber = 2;
 $parrot->carnivorous = true;
 $parrot->size = 30;
+echo $parrot->speak('fr');
+echo $parrot->speak('en');
+echo $parrot->speak();
+echo $parrot->isDangerous();
 
 $elephant = new Animal();
 $elephant->name = 'elephant';
 $elephant->pawNumber = 4;
 $elephant->threatenedLevel = 'LC';
+echo $elephant->speak('fr');
+echo $elephant->speak('en');
+echo $elephant->speak();
+echo $elephant->isDangerous();
 
 $animals = [$lion, $parrot, $elephant];
 
